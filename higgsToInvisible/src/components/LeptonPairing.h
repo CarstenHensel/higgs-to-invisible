bccab570e367a08f90fe950cb23aa18061b4ac00
Carsten Hensel <hensel.carsten@gmail.com>
2023-11-07 21:48:53 +0100

    header file done

---
+++ higgsToInvisible/src/components/LeptonPairing.h
@@ -0,0 +1,47 @@
+#pragma once
+
+// GAUDI
+#include "Gaudi/Property.h"
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+#include "edm4hep/ReconstructedParticleCollection.h"
+#include "k4FWCore/DataHandle.h"
+
+#include "TFile.h"
+#include "TH1.h"
+#include "TNtuple.h"
+
+class LeptonPairing : public GaudiAlgorithm {
+public:
+  explicit LeptonPairing(const std::string&, ISvcLocator*);
+  virtual ~LeptonPairing();
+  /**  Initialize.
+   *   @return status code
+   */
+  StatusCode initialize() final;
+  /**  Execute.
+   *   @return status code
+   */
+  StatusCode execute() final;
+  /**  Finalize.
+   *   @return status code
+   */
+  StatusCode finalize() final;
+
+private:
+  // member variable
+  DataHandle<edm4hep::ReconstructedParticleCollection> m_recoParticleCollHandle{
+      "ReconstructedParticleCollection", Gaudi::DataHandle::Reader, this};
+
+  DataHandle<edm4hep::ReconstructedParticleCollection> m_isolatedLeptonsCollHandle{
+      "IsolatedLeptonsCollection", Gaudi::DataHandle::Reader, this};
+  
+  
+  int m_event_counter = 0;	
+
+  int m_member = 0;
+  typedef std::map<std::string, TH1*> HistoMap;
+  HistoMap histos;
+  void fillHisto(std::string key, double value);
+  void fillHisto(const char *name, const char *title, int nbinsx, double xlow, double xup, double value);
+};
